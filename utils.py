cf00be6d284d8130f4fd48ddd6c411e10f3d2c92
Tianjia Yang <ytj254@163.com>
2025-04-05 18:18:24 -0400

    Add label mapping

--- utils.py
+++ utils.py
@@ -1,5 +1,34 @@
 import pandas as pd
 
+battery_chem_map = {
+    1: "Lithium-Ion Batteries",
+    2: "Nickel-Metal Hydride Batteries",
+    3: "Lead-Acid Batteries",
+    4: "Ultracapacitors",
+    5: "Other"
+}
+
+charger_type_map = {
+    "1": "Level 1",
+    "2": "Level 2",
+    "3": "DCFC",
+    "4": "Other"
+}
+
+connector_type_map = {
+    "1": "Level 1",
+    "2": "Level 2",
+    "3": "DCFC",
+    "4": "Other"
+}
+
+def map_multi_labels(series, mapping_dict):
+    return series.fillna("").apply(
+        lambda x: ", ".join(
+            [mapping_dict.get(i.strip(), i.strip()) for i in x.split(",") if i.strip()]
+        )
+    )
+
 def format_if_notna(value, placeholder="N/A"):
     """Return original value if not null; otherwise return placeholder."""
     if pd.notna(value):
