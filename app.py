625d4054810d08227f982bc05bf72ddadccea667
Tianjia Yang <ytj254@163.com>
2025-04-04 20:25:08 -0400

    Minor modifications

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ import dash_bootstrap_components as dbc
 app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 nav_items = [
-    dbc.NavItem(dbc.NavLink([html.I(className="bi bi-house me-2"), "OVERVIEW"], href="/", active="exact")),
-    dbc.NavItem(dbc.NavLink([html.I(className="bi bi-truck me-2"), "FLEET"], href="/fleet_info", active="exact")),
-    dbc.NavItem(dbc.NavLink([html.I(className="bi bi-car-front me-2"), "VEHICLE"], href="/vehicle_infor", active="exact")),
-    dbc.NavItem(dbc.NavLink([html.I(className="bi bi-battery-charging me-2"), "CHARGER"], href="/charger_info", active="exact")),
-    dbc.NavItem(dbc.NavLink([html.I(className="bi bi-wrench me-2"), "MAINTENANCE"], href="/maintenance", active="exact")),
-    dbc.NavItem(dbc.NavLink([html.I(className="bi bi-bolt me-2"), "CHARGING"], href="/charging", active="exact")),
-    dbc.NavItem(dbc.NavLink([html.I(className="bi bi-satellite-dish me-2"), "TELEMATICS"], href="/telematics", active="exact")),
-    dbc.NavItem(dbc.NavLink([html.I(className="bi bi-calendar-day me-2"), "DAILY USAGE"], href="/veh_daily_usage", active="exact")),
-    dbc.NavItem(dbc.NavLink([html.I(className="bi bi-bar-chart me-2"), "ANALYSIS"], href="/analysis", active="exact")),
+    dbc.NavItem(dbc.NavLink("OVERVIEW", href="/", active="exact")),
+    dbc.NavItem(dbc.NavLink("FLEET", href="/fleet_info", active="exact")),
+    dbc.NavItem(dbc.NavLink("VEHICLE", href="/vehicle_infor", active="exact")),
+    dbc.NavItem(dbc.NavLink("CHARGER", href="/charger_info", active="exact")),
+    dbc.NavItem(dbc.NavLink("MAINTENANCE", href="/maintenance", active="exact")),
+    dbc.NavItem(dbc.NavLink("CHARGING", href="/charging", active="exact")),
+    dbc.NavItem(dbc.NavLink("TELEMATICS", href="/telematics", active="exact")),
+    dbc.NavItem(dbc.NavLink("DAILY USAGE", href="/veh_daily_usage", active="exact")),
+    dbc.NavItem(dbc.NavLink("ANALYSIS", href="/analysis", active="exact")),
 ]
 
 navbar = dbc.NavbarSimple(
