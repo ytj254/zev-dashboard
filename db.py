086fd46a5c9da209afb3f460b7d2eb630545d634
Tianjia Yang <ytj254@163.com>
2025-04-05 18:12:22 -0400

    Add queries of vehicel and charger.

--- db.py
+++ db.py
@@ -10,3 +10,22 @@ def get_fleet_data():
         WHERE latitude IS NOT NULL AND longitude IS NOT NULL
     """
     return pd.read_sql(query, engine)
+
+def get_veh_data():
+    query = """
+        SELECT id, fleet_id, fleet_vehicle_id, make, model, year, class, curb_wt, gross_wt, rated_cap, nominal_range, nominal_eff, battery_chem, peak_power, peak_torque, towing_cap, vocation
+        FROM vehicle
+    """
+    return pd.read_sql(query, engine)
+
+def get_charger_data():
+    query = """
+        SELECT id, fleet_id, charger, charger_type, connector_type, max_power_output, dedicated_use
+        FROM charger
+    """
+    return pd.read_sql(query, engine)
+
+
+if __name__ == "__main__":
+    df = get_charger_data()
+    print(df)
